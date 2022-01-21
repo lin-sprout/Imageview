89c183dc6d08a86f71c0623aaf1bc3a2ac609077
Lin LaMonte <penelopelamonte@gmail.com>
2022-01-21 13:46:17 -0700

    Final Product

--- Imageview/ViewController.swift
+++ Imageview/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var imageview: UIImageView!
+    
+    
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageview.image = UIImage(named: "flour 2")
+        
     }
 
-
+    
+    @IBAction func onButtonTap(_ sender: UIButton) {
+        imageview.image = UIImage(named: "man copy")
+    }
+    
+    
+    @IBAction func onButtonTap2(_ sender: UIButton) {
+        imageview.image = UIImage(named: "flour 2")
+    }
+    
 }
 
